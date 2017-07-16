15b20efe2571df43c9810be4a6c25a5da0b094b8
fletch <ste.fletcher@gmail.com>
2017-07-16 10:27:09 +0100

    initial commit

---
+++ src/main/java/com/stefletcher/openshift/gradledeploymentdemo/GradleDeploymentDemoApplication.java
@@ -0,0 +1,12 @@
+package com.stefletcher.openshift.gradledeploymentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GradleDeploymentDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GradleDeploymentDemoApplication.class, args);
+	}
+}
