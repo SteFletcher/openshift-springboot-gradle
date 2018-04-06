c9220f6de87530c2dea58ccaa3667d4b9803df3e
Barclays MCA <barclaysmca@240.sub-97-1-239.myvzw.com>
2018-04-06 16:48:23 +0100

    Hezlecast example added

--- src/main/java/com/stefletcher/openshift/gradledeploymentdemo/GradleDeploymentDemoApplication.java
+++ src/main/java/com/stefletcher/openshift/gradledeploymentdemo/GradleDeploymentDemoApplication.java
@@ -2,11 +2,24 @@ package com.stefletcher.openshift.gradledeploymentdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.util.Map;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+import com.hazelcast.core.IdGenerator;
 
 @SpringBootApplication
 public class GradleDeploymentDemoApplication {
 
 	public static void main(String[] args) {
+		
 		SpringApplication.run(GradleDeploymentDemoApplication.class, args);
+		//create new hazelcast member
+		HazelcastInstance hazelcastInstance = Hazelcast.newHazelcastInstance();
+        Map<String, String> map = hazelcastInstance.getMap("data");
+        
+
+
+        
 	}
 }
--- src/main/java/com/stefletcher/openshift/gradledeploymentdemo/controllers/HelloController.java
+++ src/main/java/com/stefletcher/openshift/gradledeploymentdemo/controllers/HelloController.java
@@ -4,18 +4,67 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
+import java.util.Map.Entry;
+
+import com.hazelcast.client.HazelcastClient;
+import com.hazelcast.client.config.ClientConfig;
+import com.hazelcast.config.GroupConfig;
+import com.hazelcast.core.HazelcastInstance;
+import java.util.Map;
 
 @Controller
 @EnableAutoConfiguration
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/write/{id}")
     @ResponseBody
-    String home() {
-        return "Hello World!";
+    String write(@PathVariable("id")String id) {
+          //read it using client
+          System.out.println("parameter passed = " + id);
+		ClientConfig config = new ClientConfig();
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName("dev");
+        groupConfig.setPassword("dev-pass");
+        HazelcastInstance hazelcastInstanceClient = HazelcastClient.newHazelcastClient(config);
+        Map<String, String> map = hazelcastInstanceClient.getMap("data");
+        System.out.println("adding parameter = " + id);
+        map.put("Parameter" ,id);
+        return "Parameter added " +id;
     }
 
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(HelloController.class, args);
+    @RequestMapping("/read")
+    @ResponseBody
+    String read() {
+          //read it using client
+		ClientConfig config = new ClientConfig();
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName("dev");
+        groupConfig.setPassword("dev-pass");
+        HazelcastInstance hazelcastInstanceClient = HazelcastClient.newHazelcastClient(config);
+        Map<String, String> map = hazelcastInstanceClient.getMap("data");
+        return "Param read = " + map.get("Parameter") ;
     }
+
+    @RequestMapping("/hits")
+    @ResponseBody
+    String hits() {
+          //read it using client
+		ClientConfig config = new ClientConfig();
+        GroupConfig groupConfig = config.getGroupConfig();
+        groupConfig.setName("dev");
+        groupConfig.setPassword("dev-pass");
+        HazelcastInstance hazelcastInstanceClient = HazelcastClient.newHazelcastClient(config);
+        Map<String, String> map = hazelcastInstanceClient.getMap("data");
+        String hitsStr = map.get("hits");
+        Integer hits = 0;
+        if(hitsStr!= null){
+            hits=Integer.valueOf(hitsStr);
+            hits++;
+        } 
+        map.put("hits" ,Integer.toString(hits));
+        return "hits=" + hits;
+       
+    }
+
+   
 }
